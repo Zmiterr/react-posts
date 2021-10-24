6a262baa11bc7fa884e6c2f9c693f409232fd902
Zmiter <ozmiter@gmail.com>
2021-10-24 11:03:36 +0300

    feat: add input form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css"
 import PostList from "./components/PostList/PostList";
+import Button from "./components/UI/Button/Button";
+import Input from "./components/UI/Input/Input";
 
 function App() {
 
   return (
     <div className="App">
+        <form>
+            <Input type="text" placeholder="Add tile"/>
+            <Input type="text" placeholder="post content"/>
+            <Button>Create post</Button>
+        </form>
         <PostList title={'Post list title'}/>
     </div>
   );
