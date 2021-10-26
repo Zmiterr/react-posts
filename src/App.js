e50925b61916325989835c90e58cf07b4fc953af
Zmiter <ozmiter@gmail.com>
2021-10-27 00:15:07 +0300

    feat: create routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import "./App.css"
 import React from 'react';
 import Posts from "./components/pages/Posts";
+import {BrowserRouter, Link, Route} from "react-router-dom";
+import About from "./components/pages/About";
 
 const App = () => {
     return (
-        <div>
-            <Posts />
-        </div>
+        <BrowserRouter>
+            <div className="navbar">
+                <div className="navbar__links">
+                    <Link className="navbar__link" to={'/'}>Posts</Link>
+                    <Link className="navbar__link" to={'/about'}>About</Link>
+                </div>
+            </div>
+            <Route path={'/'} exact>
+                <Posts  />
+            </Route>
+            <Route path={'/about'}>
+                <About />
+            </Route>
+
+        </BrowserRouter>
+
     );
 };
 
